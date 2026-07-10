e0d979618020b06cf56e72f5933332afd9500fe2
nguyen842-ai <nguyen842@gmail.com>
2026-07-10 11:51:10 +0700

    Update kamMap.js

--- kamMap.js
+++ kamMap.js
@@ -4,12 +4,14 @@
 // =========================================================
 
 const kamMap = {
-  "3. SOUTH MEKONG|TRẦN ĐÌNH PHÚC": "PHẠM CHIẾN THẮNG",
+ "3. SOUTH MEKONG|TRẦN ĐÌNH PHÚC": "PHẠM CHIẾN THẮNG",
 "3. SOUTH MEKONG|PHẠM MINH SANG": "PHAN HỒ THANH TRÚC",
 "1. HCMC|ĐOÀN VĂN TUẤN": "CHÂU QUÍ",
 "2. NORTH MEKONG|PHAN VĂN SO": "HUỲNH VĂN GIÀU",
 "3. NORTH WEST|CDM TOẢN": "NGUYỄN NGỌC LONG",
 "1. HCMC|NGUYỄN THỊ KIỀU OANH": "NGÔ QUANG VINH",
 "3. SOUTH MEKONG|PHAN PHÚC SƠN": "NGUYỄN HỒNG KONG",
+"3. SOUTH MEKONG|DU MINH TÚ": "PHAN HỒ THANH TRÚC",
+"1. HCMC|TRẦN ANH KHÔI": "CHÂU QUÍ",
 
 };
