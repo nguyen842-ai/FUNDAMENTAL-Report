58bfb2a02d817d045c53085786979cd97ca6a80c
nguyen842-ai <nguyen842@gmail.com>
2026-09-04 15:06:26 +0700

    Create cdmMap.js

---
+++ cdmMap.js
@@ -0,0 +1,30 @@
+// =========================================================
+// BẢNG TỪ ĐIỂN ĐỔI TÊN KAM CŨ THÀNH KAM MỚI
+// Hướng dẫn: Viết HOA toàn bộ tên cũ bên trái, tên mới bên phải
+// =========================================================
+
+const cdmMap = {
+"NGUYỄN THỊ THỦY": "NGUYỄN THANH BÌNH",
+"NGUYỄN THỊ THỦY": "CHÂU QUÍ",
+"NGUYỄN THỊ THỦY": "NGUYỄN MINH TÂM",
+"NGUYỄN THỊ THỦY": "NGÔ QUANG VINH",
+"NGUYỄN THỊ THỦY": "NGÔ THÀNH Ý",
+"NGUYỄN THỊ THỦY": "NGUYỄN THỊ KIỀU OANH",
+"PHẠM THỊ ANH THƯ": "TRƯƠNG HÀO HIỆP",
+"PHẠM THỊ ANH THƯ": "PHẠM TRẦN QUANG",
+"PHẠM THỊ ANH THƯ": "HUỲNH VĂN GIÀU",
+"HOÀNG ANH TÚ": "NGUYỄN HỒNG KONG",
+"HOÀNG ANH TÚ": "PHAN HỒ THANH TRÚC",
+"HOÀNG ANH TÚ": "NGUYỄN TRUNG HIẾU",
+"HOÀNG ANH TÚ": "PHẠM CHIẾN THẮNG",
+"LÊ TRẦN KIM KHÁNH": "BÙI THỊ THANH THỦY",
+"LÊ TRẦN KIM KHÁNH": "LÝ ĐỖ PHƯƠNG",
+"LÊ TRẦN KIM KHÁNH": "ĐẶNG HOÀNG SƠN",
+"LÊ TRẦN KIM KHÁNH": "PHẠM HỮU HẠNH",
+"VŨ NGỌC HẢI": "BÙI CÔNG PHƯỚC",
+"VŨ NGỌC HẢI": "HỒ VĂN TIẾN",
+"PHẠM QUỐC TOẢN": "VŨ DUY HÙNG",
+"PHẠM QUỐC TOẢN": "NGUYỄN THANH QUỲNH",
+"PHẠM QUỐC TOẢN": "NGUYỄN NGỌC LONG",
+
+};
